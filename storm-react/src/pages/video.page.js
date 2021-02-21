4fa7ae158a37ad3698b12458d61d1ddbe19e1f69
kevinshih <kevinmkshih@gmail.com>
2021-02-21 12:07:34 -0800

    audio slider

--- storm-react/src/pages/video.page.js
+++ storm-react/src/pages/video.page.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import YoutubePlayer from '../components/YoutubePlayer'
+import React, {useState} from 'react'
+import YoutubePlayer from '../components/YouTubePlayer'
 import spotifyAuth from '../utils/spotifyAuth'
 import ChatRoom from '../container/chatRoom'
 
 import classes from './video.module.scss'
 
 function Video(props) {
-    console.log(props.user)
+    const [radioSrc, setSrc] = useState("https://tunein.com/embed/player/s11985?autoplay=true")
+    console.log(props.router)
     const video_id = props.router.location.search.replace("?v_id=", "")
     console.log(video_id)
     window.onSpotifyWebPlaybackSDKReady = () => {
         console.log('spotify ready')
+    }
+
+    let i = document.querySelector('input')
+
+
+
+    function sliderChange(station) {
+        setSrc(station)
     }
 
     return (
@@ -19,7 +28,30 @@ function Video(props) {
                 <YoutubePlayer video_id={video_id}/>
             </div>
             <div>
-                <iframe class={classes.RadioPlayer} src={"https://tunein.com/embed/player/s55425/?autoplay=true"} scrolling={"no"} frameBorder={"no"} title="radio" autoplay></iframe>
+                <iframe class={classes.RadioPlayer} src={radioSrc} scrolling={"no"} frameBorder={"no"} title="radio"/>
+            </div>
+            <div>
+                <p>Radio Selector</p>
+            </div>
+            <div>
+                <input type="range" min="0" max="4" onChange={(e) => {
+
+                        let val = e.target.value;
+                        if (val == 1) {
+                            sliderChange("https://tunein.com/embed/player/s31258?autoplay=true");
+                        } else if (val == 2) {
+                            sliderChange("https://tunein.com/embed/player/s55425?autoplay=true");
+                        } else if (val == 3) {
+                            sliderChange("https://tunein.com/embed/player/s31214?autoplay=true");
+                        } else if (val == 4) {
+                            sliderChange("https://tunein.com/embed/player/s31128?autoplay=true");
+                        } else if (val == 5) {
+                            sliderChange("https://tunein.com/embed/player/s11985?autoplay=true")
+                        }
+
+
+                    console.log(e.target.value)
+                }}/>
             </div>
             <div >
                 <ChatRoom user={props.user} />
