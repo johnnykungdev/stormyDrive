ce6b9cd84c93d3fe94f7c8d1d7a79abbf13d3b44
Che-Yi Kung <kjohnathan.engi@gmail.com>
2021-02-21 13:14:24 -0800

    enalble click on vid

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('listen on port 8000')
 })
